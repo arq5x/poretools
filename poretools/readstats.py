d6755a7ab6ddef434cac7c55df1326804d7755f9
Nick Loman <n.j.loman@bham.ac.uk>
2014-09-10 07:52:06 +0000

    optimised readstats

--- poretools/readstats.py
+++ poretools/readstats.py
@@ -10,12 +10,17 @@ def run(parser, args):
 		channel_number = fast5.get_channel_number()
 		read_number = fast5.get_read_number()
 
-		template_len = 0
-		if fast5.get_template_events() is not None:
-			template_len = len(fast5.get_template_events())
-		complement_len = 0
-		if fast5.get_complement_events() is not None:
-			complement_len = len(fast5.get_complement_events())
+		template_events = fast5.get_template_events()
+		if template_events is not None:
+			template_len = len(template_events)
+		else:
+			template_len = 0
+
+		complement_events = fast5.get_complement_events()
+		if complement_events is not None:
+			complement_len = len(complement_events)
+		else:
+			complement_len = 0
 
 		print "%s\t%s\t%s\t%s\t%s" % (start_time, channel_number, read_number, template_len, complement_len)
 
