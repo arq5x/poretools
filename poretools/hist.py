ce0a8f1ef81cee2272c9b320dc46b7b4c81f86db
Brent Pedersen (brentp) <bpederse@gmail.com>
2015-06-18 14:48:57 -0600

    start of rpy2 removal. see #53

--- poretools/hist.py
+++ poretools/hist.py
@@ -1,65 +1,29 @@
 import sys
 import time
+from matplotlib import pyplot as plt
+import seaborn as sns
 import Fast5File
-import rpy2.robjects as robjects
-import rpy2.robjects.lib.ggplot2 as ggplot2
-from rpy2.robjects.packages import importr
 from watchdog.observers import Observer
 from collections import Counter
 
-#logging
 import logging
 logger = logging.getLogger('poretools')
 logger.setLevel(logging.INFO)
 
 def plot_hist(sizes, args):
     """
-    Use rpy2 to plot a histogram of the read sizes
+    plot a histogram of the read sizes
     """
-    r = robjects.r
-    r.library("ggplot2")
-    grdevices = importr('grDevices')
+    sizes = [s for s in sizes  if args.min_length < s < args.max_length]
 
-    sizes = robjects.IntVector([s for s in sizes \
-                if s < args.max_length and s > args.min_length])
-
-    sizes_min = min(sizes)
-    sizes_max = max(sizes)
-
-    binwidth = (sizes_max - sizes_min) / args.num_bins
-
-    d = {'sizes' : sizes}
-    df = robjects.DataFrame(d)
-
-    # plot
-    gp = ggplot2.ggplot(df)
-
-    if not args.theme_bw:
-        pp = gp + ggplot2.aes_string(x='sizes') \
-                + ggplot2.geom_histogram(binwidth=binwidth)
-    else:
-        pp = gp + ggplot2.aes_string(x='sizes') \
-            + ggplot2.geom_histogram(binwidth=binwidth) \
-            + ggplot2.theme_bw()            
+    if args.theme_bw:
+        sns.set_style("whitegrid")
+    plt.hist(sizes, args.num_bins)
 
     if args.saveas is not None:
-        plot_file = args.saveas
-        if plot_file.endswith(".pdf"):
-            grdevices.pdf(plot_file, width = 8.5, height = 8.5)
-        elif plot_file.endswith(".png"):
-            grdevices.png(plot_file, width = 8.5, height = 8.5, 
-                units = "in", res = 300)
-        else:
-            logger.error("Unrecognized extension for %s!" % (plot_file))
-            sys.exit()
-
-        pp.plot()
-        grdevices.dev_off()
+        plt.savefig(args.saveas)
     else:
-        pp.plot()
-        # keep the plot open until user hits enter
-        print('Type enter to exit.')
-        raw_input()
+        plt.show()
 
 def run(parser, args):
     sizes = []
