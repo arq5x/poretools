5acedf6fab1607f8dea0cdc46df66daea870a275
Mike Lindberg <ml4wc@users.noreply.github.com>
2015-06-30 12:57:02 -0400

    Update fastq.py

--- poretools/fastq.py
+++ poretools/fastq.py
@@ -25,8 +25,8 @@ def run(parser, args):
 		for fa in fas:
 			if fa is None or \
 			len(fa.seq) < args.min_length or \
-			len(fa.seq) > args.max_length and \
-			args.max_length > 0:			
+			(len(fa.seq) > args.max_length and \
+			args.max_length > 0):			
 				continue
 
 			print fa
